0b51b82f1e8e34ac66d237da3a3a067df1d9625c
P40b0s <uranius666@gmail.com>
2024-06-17 16:20:55 +0300

    сделал возможность инициализации нескольких клиентов

--- service/src/client.rs
+++ service/src/client.rs
@@ -1,78 +1,123 @@
-use std::{collections::HashMap, sync::{atomic::AtomicBool, Arc}};
-use futures::{future::BoxFuture, Future, FutureExt, SinkExt};
-use futures_channel::mpsc::{unbounded, UnboundedReceiver, UnboundedSender};
-use futures_util::{future, pin_mut, StreamExt, TryStreamExt};
-use logger::{backtrace, debug, error, warn};
-use once_cell::{sync::{Lazy, OnceCell}};
+use std::collections::HashMap;
+use anyhow::Context;
+use futures::Future;
+use futures_channel::mpsc::{unbounded, UnboundedSender};
+use futures_util::{future, pin_mut, StreamExt};
+use logger::{backtrace,  error};
+use once_cell::sync::OnceCell;
 use tokio::sync::Mutex;
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
-use crate::{message::Converter, retry};
-//TODO Пока могу обслужить только клиента в единственном экземпляре
-static SENDER: OnceCell<Mutex<UnboundedSender<Message>>> = OnceCell::new();
-static IS_CONNECTED: AtomicBool = AtomicBool::new(false);
+use crate::retry;
+
+static SENDER: OnceCell<Mutex<HashMap<String, UnboundedSender<Message>>>> = OnceCell::new();
+static IS_CONNECTED: OnceCell<Mutex<HashMap<String, bool>>> = OnceCell::new();
 
 
-
-pub trait Client<T: Converter>
+pub trait Client<T> where T: serde::Serialize + Send + Sync, for <'de> T : serde::Deserialize<'de> + Sized + Send
 {
+    fn get_id() -> &'static str;
     fn start_client<F>(addr: &str, f:F)  -> impl Future<Output = ()> + Send
     where F:  Send + Sync + Copy + 'static + Fn(T)
     {
         let addr = addr.to_owned();
-        async move 
+        let cli_id = Self::get_id();
+        async move
         {
             tokio::spawn(async move
             {
                 loop
                 {
-                    start(addr.clone(), f,0, 15).await;
+                    start(cli_id, addr.clone(), f,0, 15).await;
                 }
             });
+            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
         }
     }
-    fn is_connected() -> bool
+    fn is_connected() -> impl Future<Output = bool> + Send
     {
-        IS_CONNECTED.load(std::sync::atomic::Ordering::SeqCst)
+        async 
+        {
+            let guard = IS_CONNECTED.get_or_init(|| Mutex::new(HashMap::new())).lock().await;
+            if let Some(curr) = guard.get(Self::get_id())
+            {
+                return *curr
+            }
+            else 
+            {
+                false
+            }
+        }
     }
     fn send_message(wsmsg: T) -> impl Future<Output = ()> + Send
     {
-        async {
-        if !IS_CONNECTED.load(std::sync::atomic::Ordering::SeqCst)
+        async move 
         {
-            error!("Ошибка отправки сообщения, нет подключения к серверу")
+            if Self::is_connected().await
+            {
+                let mut message: Vec<u8> = Vec::new();
+                let _ = serde_json::to_writer(&mut message, &wsmsg);
+                let message = Message::Binary(message);
+                let sender = SENDER.get().unwrap().lock().await;
+                let curr_sender = sender.get(Self::get_id()).unwrap();
+                let sended = curr_sender.unbounded_send(message);
+                if sended.is_ok()
+                {
+                    return ();
+                }
+                else
+                {
+                    logger::error!("Ошибка отправки сообщения {}", sended.err().unwrap());
+                    return ();
+                }
+            }
+            else 
+            {
+                logger::error!("Ошибка отправки сообщения, нет подключения к серверу");
+                return ();
+            }
         }
-        else 
-        {
-            let message = wsmsg.to_binary();
-            let message = Message::Binary(message);
-            let _ = SENDER.get().unwrap().lock().await.unbounded_send(message);
-        }
-    }
     }
     fn ping() -> impl Future<Output = ()> + Send
     {
-        async {
-        if IS_CONNECTED.load(std::sync::atomic::Ordering::SeqCst)
+        async 
         {
-            let msg = Message::Ping([12].to_vec());
-            let _ = SENDER.get().unwrap().lock().await.unbounded_send(msg);
+            let guard = IS_CONNECTED.get_or_init(|| Mutex::new(HashMap::new())).lock().await;
+            if let Some(curr) = guard.get(Self::get_id())
+            {
+                if *curr == true
+                {
+                    let msg = Message::Ping([12].to_vec());
+                    let sender = SENDER.get().unwrap().lock().await;
+                    let curr_sender = sender.get(Self::get_id()).unwrap();
+                    let _ = curr_sender.unbounded_send(msg);
+                }
+                else
+                {
+                    error!("Ошибка отправки сообщения, нет подключения к серверу");
+                }
+            }
+            else 
+            {
+                error!("Ошибка отправки сообщения, нет подключения к серверу");
+            }
         }
-    }
     }
 }
 
-async fn start<F, T: Converter>(addr: String, f:F, attempts: u8, delay: u64) -> bool
-where F:  Send + Copy + 'static + Fn(T)
+async fn start<F, T>(cli_id: &str, addr: String, f:F, attempts: u8, delay: u64) -> bool 
+where T: serde::Serialize + Send, for <'de> T : serde::Deserialize<'de> + Sized + Send, F:  Send + Copy + 'static + Fn(T)
 {
     let (sender, local_receiver) = unbounded::<Message>();
     if let Some(s) = SENDER.get()
     {
         let mut guard = s.lock().await;
-        *guard = sender.clone();
+        guard.insert(cli_id.to_owned(), sender.clone());
     }
     else
     {
-        let _ = SENDER.set(Mutex::new(sender.clone()));
+        let mut hm: HashMap<String, UnboundedSender<Message>> = HashMap::new();
+        hm.insert(cli_id.to_owned(), sender.clone());
+        let _ = SENDER.set(Mutex::new(hm));
     }
     let connected = retry(attempts, delay, || connect_async(&addr)).await;
     if let Err(e) = connected.as_ref()
@@ -80,12 +125,14 @@ where F:  Send + Copy + 'static + Fn(T)
         error!("Ошибка подключения к серверу websocket по адресу {} -> {}", &addr, e.to_string());
         return false;
     }
-    IS_CONNECTED.store(true, std::sync::atomic::Ordering::SeqCst);
+    let mut conn = IS_CONNECTED.get_or_init(|| Mutex::new(HashMap::new())).lock().await;
+    conn.insert(cli_id.to_owned(), true);
+    drop(conn);
     let (ws_stream, resp) = connected.unwrap();
-    debug!("Рукопожатие с сервером успешно");
+    logger::debug!("Рукопожатие с сервером успешно");
     for h in resp.headers()
     {
-        debug!("* {}: {}", h.0.as_str(), h.1.to_str().unwrap());
+        logger::debug!("* {}: {}", h.0.as_str(), h.1.to_str().unwrap());
     }
     let (write, read) = ws_stream.split();
     //сообщения полученные по каналу local_receiver'ом форвардятся прямо в вебсокет
@@ -99,7 +146,7 @@ where F:  Send + Copy + 'static + Fn(T)
             {
                 if message.is_binary()
                 {
-                    let msg = T::from_binary(&message.into_data());
+                    let msg = serde_json::from_slice::<T>(&message.into_data()).with_context(|| format!("Данный объект отличается от того который вы хотите получить"));
                     if let Ok(m) = msg
                     {
                         f(m)
@@ -119,35 +166,16 @@ where F:  Send + Copy + 'static + Fn(T)
     };
     pin_mut!(send_to_ws, from_ws);
     future::select(send_to_ws, from_ws).await;
-    IS_CONNECTED.store(false, std::sync::atomic::Ordering::SeqCst);
-    warn!("Сервер недоступен! повторная попытка подключения");
+    let mut conn = IS_CONNECTED.get().unwrap().lock().await;
+    conn.remove(cli_id);
+    let mut snd = SENDER.get().unwrap().lock().await;
+    snd.remove(cli_id);
+    logger::warn!("Сервер недоступен! повторная попытка подключения");
     return false;
 }
 
 #[cfg(test)]
 mod test
 {
-    use logger::debug;
-    //use crate::WebsocketMessage;
-    use super::Client;
-
-    enum Packet
-    {
-        Test1
-    }
-    // #[tokio::test]
-    // async fn test_client()
-    // {
-    //     logger::StructLogger::initialize_logger();
-    //     super::Client::start_client("ws://127.0.0.1:3010/", receiver).await;
-    //     loop 
-    //     {
-    //         tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
-    //         let _ = Client::ping().await;
-    //     }
-    // }
-    // fn receiver(ms: WebsocketMessage)
-    // {
-    //     debug!("клиенту поступило новое сообщение {:?}", ms);
-    // }
+    
 }
